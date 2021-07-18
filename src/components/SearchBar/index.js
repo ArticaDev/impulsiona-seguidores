46324cc3eb8f02458a547d81ffca0dbccfea7d39
Lucas Lima <limalucas454@gmail.com>
2021-07-18 01:24:33 -0300

    putting search button inside searchbar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import style from "./styles";
-import { Search } from 'akar-icons';
+import { Search } from "akar-icons";
 
 const SearchBar = ({ className }) => (
   <>
     <div className={className}>
-      <div className="container flex flex-row mt-4 px-6 justify-center items-center bg-grey">
-      <input className="w-screen max-w-5xl h-10 p-4 bg-white text-link" type="text" placeholder="Pesquise por um conteúdo"></input>
-      <button>
-      <Search className="text-link ml-2" size={24} />
-      </button>
+      <div className="w-full flex flex-row mt-4 px-6 justify-center items-center">
+        <input
+          className="w-screen max-w-5xl h-10 p-4 bg-white text-link"
+          type="text"
+          placeholder="Pesquise por um conteúdo"
+        ></input>
+        <button>
+          <Search className="text-link -ml-10" size={24} />
+        </button>
       </div>
     </div>
   </>

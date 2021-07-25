ff3d84e90d015d4c0bef9006fe4b2569671e3274
MarcosN <marcosnery.comp@gmail.com>
2021-07-25 01:54:27 -0300

    feat: proxying http images to https

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -15,7 +15,7 @@ const BlogPost = ({ data, location, className }) => {
       <FloatingButtons  pageUrl={url} />
       <div className={className}>
         <div className="container px-6 lg:px-20 mt-8 topo-blog">
-          <img className="container thumbnail" src={post.featuredImage.node.mediaItemUrl}></img>
+          <img alt="thumbnail" className="container thumbnail" src={'//images.weserv.nl/?'+post.featuredImage.node.mediaItemUrl}></img>
 
           <h1 className="text-link text-4xl text-center mb-8 mt-5">
             {post.title}

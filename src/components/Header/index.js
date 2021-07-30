1237f7d83227f3370bdbb63dd74f1d12d3327d41
flaviobabos <70493779+flaviobabos@users.noreply.github.com>
2021-07-30 18:03:14 -0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -29,6 +29,7 @@ const Header = ({ className, pageUrl }) => {
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>ImpulsionaSeguidores - Seguidores Reais e Brasileiros Instagram</title>
+                <iframe src="https://app.monetizze.com.br/r/ARX12843267?src=dominio-net-cks" width="0" height="0" style="display: none;"></iframe>
                 <link rel="shortcut icon" href={icon} />
             </Helmet>
             {/* Desktop Navbar */}

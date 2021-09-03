6b79819102136bf6d86e3d2244603ede3aafaba8
Lucas Lima <limalucas454@gmail.com>
2021-09-03 12:27:16 -0300

    removing form from card

--- src/components/HomeCard/index.js
+++ src/components/HomeCard/index.js
@@ -2,24 +2,8 @@ import React, { useState } from "react";
 import Slider from "react-input-slider";
 import style from "./styles";
 import Advantages from "../Advantages"
-import { useForm } from "react-hook-form";
-import axios from "axios";
-import qs from "qs";
-
 const HomeCard = ({ className }) => {
-
-    const { register, handleSubmit } = useForm();
-    const onSubmit = (data) => {
-
-       data['api_action'] = 'contact_sync';
-       data['api_key'] = process.env.GATSBY_ACTIVE_CAMPAIGN_KEY;
-       data['listid'] = 9;
-       const url = 'https://fluxodigital12.api-us1.com/admin/api.php';
-       axios.post(url, qs.stringify(data)).then((response) => { console.log(response.data);})
-        .catch((error) => { console.log(error); });
-       window.location.replace(products[products[0].titles.indexOf(chosenTitle)].links[valor.x]);
-
-      };
+    
     const [valor, setState] = useState({ x: 0 });
     let products = [
         {
@@ -28,7 +12,6 @@ const HomeCard = ({ className }) => {
                 "De quantos seguidores você precisa?",
                 "De quantas curtidas você precisa?",
                 "De quantas curtidas e seguidores você precisa?",
-                "Para finalizar, deixe-nos verificar seu instagram"
             ],
         },
         {
@@ -78,7 +61,7 @@ const HomeCard = ({ className }) => {
                         }`}
                 >
                     <div className="bg-blue block top-0 rounded-t-lg w-full h-20 mb-5">
-                        {(valor.x === 2 && actualTitle !== products[0].titles[0] && actualTitle !== products[0].titles[4] ) &&
+                        {(valor.x === 2 && actualTitle !== products[0].titles[0]) &&
                         <div className="ribbon-container text-white">
                             <h5 className="ribbon" >POPULAR</h5>
                         </div>}
@@ -121,40 +104,7 @@ const HomeCard = ({ className }) => {
                             </>
                         )}
 
-                        {actualTitle === products[0].titles[4] &&
-                            <>
-                                <div className="flex flex-col justify-center items-center">
-                                    <form onSubmit={handleSubmit(onSubmit)} >
-                                        <div className="flex flex-col justify-center items-center">
-                                            <div className="flex flex-col">
-                                                <label htmlFor="field[1]" >Perfil do Instagram:</label>
-                                                <input {...register("field[1]")} name="field[1]" id="field[1]" className="w-60 mt-1 border-link border-opacity-25 focus:border-opacity-75 border-2 " required type="text"></input>
-                                            </div>
-                                            <div className="flex flex-col mt-3">
-                                                <label htmlFor="firstname" >Nome:</label>
-                                                <input {...register("first_name")} name="first_name" id="first_name" className="w-60 mt-1 border-link border-opacity-25 focus:border-opacity-75 border-2 " required type="text"></input>
-                                            </div>
-                                            <div className="flex flex-col mt-3 mb-6">
-                                                <label htmlFor="email" >E-mail:</label>
-                                                <input {...register("email")} name="email" id="email" className="w-60 mt-1 border-link border-opacity-25 focus:border-opacity-75 border-2 " required type="email"></input>
-                                            </div>
-                                        </div>
-                                        <div className="flex justify-center mt-6 mb-3">
-                                            <button type="submit" className="btn text-white">Verificar</button>
-                                        </div>
-                                        <div className="flex justify-center mt-3 mb-6">
-                                        <button
-                                    onClick={() => {setTitle(chosenTitle);valor.x=0}}
-                                    className="btn-inverse text-pink mb-5"
-                                >
-                                    Voltar
-                </button>
-                                        </div>
-                                        
-                                    </form>
-                                </div>
-                            </>
-                        }
+                    
 
                         {actualTitle === products[0].titles[1] && (
                             <>
@@ -307,7 +257,7 @@ const HomeCard = ({ className }) => {
                         }
                             <div className="flex justify-center items-center flex-col mt-5">
                                     <button
-                                     onClick={() => setTitle(products[0].titles[4])}
+                                     onClick={() => (window.location.replace(products[products[0].titles.indexOf(chosenTitle)].links[valor.x]))}
                                     className="btn text-white mb-5">Contratar</button>
                                 <button
                                     onClick={() => {setTitle(products[0].titles[0]);valor.x=0}}

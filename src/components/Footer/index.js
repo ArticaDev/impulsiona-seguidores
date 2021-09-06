94fc72384219df426fab8b619977b6486dabfe12
Lucas Lima <limalucas454@gmail.com>
2021-09-06 10:26:14 -0300

    refacting footer with placeholder linkage

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,18 +5,42 @@ import img from "../../images/seg.png";
 const Footer = ({ className }) => (
   <>
     <div className={className}>
-      <div className="h-64 bg-black flex items-center justify-items-center">
-        <div className="grid-rows-2 mx-auto">
-          <img className="h-16" style={{height: "4rem"}} alt="seguranca do site" src={img}></img>
-          <h5 className="text-center text-white text-lg mt-5">
-            Impulsiona Seguidores - 2021
-          </h5>
-          <p className="text-center text-white text-lg mt-2">
-            Desenvolvido por&nbsp;
-            <a className="text-link" href="https://articadev.com">
-              Artica Dev
+      <div className="h-64 bg-black grid lg:grid-flow-col lg:auto-cols-3 grid-flow-row auto-rows-3">
+        <div className="place-self-center lg:m-0 mt-1 mb-3">
+          <a href="mailto:contato@impulsionaseguidores.net">
+            <h5 className="text-white">contato@impulsionaseguidores.net</h5>
+          </a>
+        </div>
+        <div className="place-self-center lg:m-0 mt-1 mb-3">
+          <div className="mb-2">
+            <h5 className="text-white text-lg m-0">
+              Impulsiona Seguidores - 2021
+            </h5>
+          </div>
+          <div className="mb-1">
+            <a href="/" className="text-white">
+              Política de Privacidade&nbsp;|&nbsp;
             </a>
-          </p>
+            <a href="/" className="text-white">
+              Termos de Uso
+            </a>
+          </div>
+          <div>
+            <p className="text-center text-white text-lg">
+              Desenvolvido por&nbsp;
+              <a className="text-link" href="https://articadev.com">
+                Artica Dev
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="place-self-center lg:m-0 mt-1 mb-3">
+          <img
+            className="h-16"
+            style={{ height: "4rem" }}
+            alt="seguranca do site"
+            src={img}
+          ></img>
         </div>
       </div>
     </div>

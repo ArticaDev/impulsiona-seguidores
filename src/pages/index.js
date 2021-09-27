cba5ad74cf9faf9a38dbffe885b2e48c0e7104e0
Lucas Lima <limalucas454@gmail.com>
2021-09-27 00:51:14 -0300

    improving query and removing unused variables

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,39 @@
-import React from 'react'
-import { graphql} from "gatsby" 
-import Layout from "../components/Layout"
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import SectionWarranty from '../components/SectionWarranty'
-import SectionAbout from '../components/SectionAbout'
-import SectionContact from '../components/SectionContact'
-import SectionBenefits from '../components/SectionBenefits'
-import SectionHome from '../components/SectionHome'
-import FloatingButtons from '../components/FloatingButtons'
-import BlogSection from '../components/BlogSection'
-import SectionFaq from '../components/SectionFaq'
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import SectionWarranty from "../components/SectionWarranty";
+import SectionAbout from "../components/SectionAbout";
+import SectionContact from "../components/SectionContact";
+import SectionBenefits from "../components/SectionBenefits";
+import SectionHome from "../components/SectionHome";
+import FloatingButtons from "../components/FloatingButtons";
+import BlogSection from "../components/BlogSection";
+import SectionFaq from "../components/SectionFaq";
 
-export default function Index({data, location}) {
-    const url = location.href ? location.href : '';
-    const allPosts = data.allWpPost.nodes
-    const fewPosts = [...allPosts.slice(-3)]
-    return (
-        <Layout>
-            <Header pageUrl={url} />
-            <FloatingButtons pageUrl={url} />
-            <SectionHome />
-            <SectionAbout />
-            <SectionBenefits />
-            <SectionWarranty />
-            <SectionFaq />
-            <SectionContact />
-            <BlogSection fewPosts={fewPosts} />
-            <Footer />
-        </Layout>
-    )
+export default function Index({ data, location }) {
+  const url = location.href ? location.href : "";
+  const fewPosts = data.allWpPost.nodes;
+  return (
+    <Layout>
+      <Header pageUrl={url} />
+      <FloatingButtons pageUrl={url} />
+      <SectionHome />
+      <SectionAbout />
+      <SectionBenefits />
+      <SectionWarranty />
+      <SectionFaq />
+      <SectionContact />
+      <BlogSection fewPosts={fewPosts} />
+      <Footer />
+    </Layout>
+  );
 }
 
 export const pageQuery = graphql`
   query {
-    allWpPost(sort: { fields: [date] }) {
+    allWpPost(sort: { fields: [date], order: DESC }, limit: 3) {
       nodes {
         title
         excerpt
@@ -47,4 +46,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
